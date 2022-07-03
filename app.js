f5abeefbf59363c9952f28923ae1aa09f06c9739
Igor <mr.ryaboff2013@yandex.ru>
2022-07-03 18:58:44 +0600

    Im lazy to remember what i've changed

--- app.js
+++ app.js
@@ -32,23 +32,18 @@ app.use((req, resp, next) => {
     resp.setHeader('Access-Control-Allow-Origin', '*');
     next();
 });
-let wsApp = ExpressWS(app, rawServer);
+ExpressWS(app, rawServer);
 let sockets = [];
 app.ws('/', (ws, req) => {
-    log('ws opened');
     sockets.push(ws);
     ws.on('close', () => {
         let idx = sockets.indexOf(ws);
         if (idx != -1) sockets.splice(idx, 1);
-        log('ws closed');
     });
-    sendIncomingPhonesCount();
 });
-
-function sendIncomingPhonesCount() {
-    let data = connectedPhones.length == 0 ? incomingPhones.length : 0;
-    massSend('inc:' + data);
-}
+app.post('/hive', (req, resp) => {
+    //
+});
 function massSend(data) {
     sockets.forEach(s => s.send(data));
 }
@@ -57,21 +52,34 @@ function question(txt) {
     return new Promise(resolve => rl.question(txt, ans => resolve(ans)));
 }
 
-let logFilePath;
+let logsDir;
+const logsDirCachePath = path.resolve(require('os').homedir(), '.kch-wp-logsDir');
+if (fs.existsSync(logsDirCachePath)) logsDir = fs.readFileSync(logsDirCachePath).toString();
+log('Logs dir:', logsDir || 'Not set');
+
 let position = 0;
-const regexp = /^[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{4} \[THREAD-[0-9]{1}\] >>> CounterPathPhone:client_OnCallStatus: sip:/g;
-async function init() {
-    logFilePath = (await question('Log file path: ')).replace(/"/g, '');
+let usedCIDs = [];
+let fsWatcher;
+const phoneRegex = /^[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{4} \[THREAD-[0-9]{1}\] >>> CounterPathPhone:client_OnCallStatus: sip:/g;
+async function loadLogFile() {
+    if (!logsDir || !fs.existsSync(logsDir)) return log('Invalid logs dir');
+    let logFilePath = fs.readdirSync(logsDir).filter(fn => fn.endsWith('.console')).map(fn => path.resolve(logsDir, fn)).sort((a, b) => {
+        a = fs.statSync(a).mtime;
+        b = fs.statSync(b).mtime;
+        return b - a;
+    })[0];
     control();
     if (logFilePath == 'dbg' || !logFilePath) return log('Debugging');
     if (!fs.existsSync(logFilePath)) {
         console.log('File does not exist');
         process.exit(0);
     }
+    log('Log file:', logFilePath);
     position = fs.statSync(logFilePath).size;
     let fd = fs.openSync(logFilePath);
+    if (fsWatcher) fsWatcher.close();
 
-    fs.watch(logFilePath, {}, () => {
+    fsWatcher = fs.watch(logFilePath, {}, () => {
         let newSize = fs.statSync(logFilePath).size;
         if (newSize != position) {
             let buf = Buffer.alloc(1024 * 16);
@@ -79,11 +87,25 @@ async function init() {
             let raw = buf.toString().replace(/\u0000+$/gm, '');
             let lines = raw.split('\r\n');
             lines.forEach(ln => {
-                if (regexp.test(ln)) {
-                    ln = ln.replace(regexp, '').replace('@192.168.93.254', '');
+                if (phoneRegex.test(ln)) {
+                    ln = ln.replace(phoneRegex, '').replace('@192.168.93.254', '');
                     let phone = ln.split(' ')[0];
                     let status = ln.split(' ')[1];
                     action(phone, status);
+                }
+                else if (ln.indexOf('pc-take-order') != -1) {
+                    ln = ln.split(' >>> ').slice(1).join(' >>> ');
+                    let json;
+                    try {
+                        json = JSON.parse(ln);
+                    }
+                    catch { return; }
+                    if (!json['pc-take-order']) return;
+
+                    let cid = +json['pc-take-order']._CID_;
+                    if (usedCIDs.indexOf(cid) != -1) return;
+                    usedCIDs.push(cid);
+                    massSend('order');
                 }
             });
             position = newSize;
@@ -91,54 +113,18 @@ async function init() {
     });
     log('Listening...');
 }
-init();
 
 function action(phone, status) {
     switch (status) {
-        case 'ECS_Incoming': case 'ECS_Dialing': case 'i': case '4':
-            incoming(phone);
+        case 'ECS_Incoming': case 'i': case '4':
+            massSend(`action:${phone} incoming`);
             break;
-        case 'ECS_Connected': case 'c': case '1': case '2': case '5':
-            connected(phone);
+        case 'ECS_Connected': case 'ECS_Dialing': case 'c': case '1': case '2': case '5':
+            massSend(`action:${phone} connected`);
             break;
         case 'ECS_Disconnected': case 'd': case '6':
-            disconnected(phone);
+            massSend(`action:${phone} disconnected`);
             break;
-    }
-    sendIncomingPhonesCount();
-}
-
-let incomingPhones = [];
-let connectedPhones = [];
-function incoming(phone) {
-    const idxInc = incomingPhones.indexOf(phone);
-    const idxCon = connectedPhones.indexOf(phone);
-
-    if (idxInc == -1 && idxCon == -1) {
-        incomingPhones.push(phone);
-        log('Incoming ' + phone);
-    }
-}
-function connected(phone) {
-    const idxInc = incomingPhones.indexOf(phone);
-    const idxCon = connectedPhones.indexOf(phone);
-
-    if (idxInc != -1) incomingPhones.splice(idxInc, 1);
-    if (idxCon == -1) {
-        connectedPhones.push(phone);
-        massSend('con');
-        log('Connected ' + phone);
-    }
-}
-function disconnected(phone) {
-    const idxInc = incomingPhones.indexOf(phone);
-    const idxCon = connectedPhones.indexOf(phone);
-    
-    if (idxInc != -1) incomingPhones.splice(idxInc, 1);
-    if (idxCon != -1) connectedPhones.splice(idxCon, 1);
-    if (idxCon != idxInc) {
-        if (idxCon != -1) massSend('discon');
-        log('Disconnected ' + phone);
     }
 }
 
@@ -147,16 +133,26 @@ async function control() {
     let cmd = txt.split(' ')[0];
     let args = txt.split(' ').slice(1);
     switch (cmd) {
-        case 'reset':
-            incomingPhones = [];
-            connectedPhones = [];
-            sendIncomingPhonesCount();
-            massSend('discon');
+        case 'r': case 'reset':
+            massSend('reset');
             break;
         case 'a':
             if (args[0] && args[1]) action(args[0], args[1]);
             else log('a', args);
             break;
+        case 'o':
+            massSend('order');
+            break;
+        case 'l':
+            loadLogFile();
+            break;
+        case 'd':
+            let dir = (await question('Logs dir:')).replace(/"/g, '');
+            logsDir = dir;
+            fs.writeFileSync(logsDirCachePath, dir);
+            log('Set logs dir to', dir);
+            break;
     }
     control();
 }
+control();
